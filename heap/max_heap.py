6583574a935907d2042549957250567ad47ee665
ltrii <lelandtr@gmail.com>
2019-05-08 15:46:03 -0700

    Heap complete

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,13 +3,15 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
-    # delete = self.storage[0]
-    # self.storage[0] = self.storage[len(self.storage) - 1]
-    # self.storage.pop()
-    pass
+    delete = self.storage[0]
+    self.storage[0] = self.storage[len(self.storage) - 1]
+    self.storage.pop()
+    self._sift_down(0)
+    return delete
 
   def get_max(self):
     if self.storage[0] != None:
@@ -28,4 +30,19 @@ class Heap:
         index = (index - 1) // 2
 
   def _sift_down(self, index):
-    pass
+    storage = self.storage
+    maxid = None
+    if index * 2 + 1 >= self.get_size():
+        return
+    elif index * 2 + 2 >= self.get_size():
+        maxid = index * 2 + 1
+    elif storage[index * 2 + 1] > storage[index * 2 + 2]:
+        maxid = index * 2 + 1
+    else:
+        maxid = index * 2 + 2
+
+    if storage[index] < storage[maxid]:
+        storage[index], storage[maxid] = storage[maxid], storage[index]
+        self._sift_down(maxid)
+    else:
+        return
