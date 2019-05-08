09fa9186fbc99b36f944c62de7dc14578b12e920
ltrii <lelandtr@gmail.com>
2019-05-08 12:31:34 -0700

    Some heap work

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -6,16 +6,23 @@ class Heap:
     pass
 
   def delete(self):
-    pass
+
 
   def get_max(self):
-    pass
+    if self.storage[0]:
+      return self.storage[0]
+    else
+      return None
 
   def get_size(self):
     pass
 
   def _bubble_up(self, index):
-    pass
+    while (index - 1) // 2 >= 0:
+        if self.storage[(index - 1) // 2] < self.storage[index]:
+            self.storage[index], self.storage[(
+                index - 1) // 2] = self.storage[(index - 1) // 2], self.storage[index]
+        index = (index - 1) // 2
 
   def _sift_down(self, index):
     pass
